434eb2c36a8a4ed97b00ae1cbec951fbc5b8395a
kjiangab <kjiangab@csl4wk05.win2k.cs.ust.hk>
2019-02-21 09:58:39 +0800

    i make a change

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -4,7 +4,12 @@
 public class Library {
 	public static void main(String arg[])
 	{
+		Library lib=new Library();
+		if(lib.someLibraryMethod())
+		{
 		System.out.println("when there is a fire, commit and push.");
+	
+		}
 	}
     public boolean someLibraryMethod() {
         return true;
